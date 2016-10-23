160a886f2e3f11f94507bbbf271a293bb9e5f7d3
Jaha96 <j_jaha28@yahoo.com>
2016-10-23 14:16:04 +0800

    Шинээр үүсгэж XML-д элемент нэмж харж болдог болгов

---
+++ WriteXML/default.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="WriteXML._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="Style.css" rel="stylesheet" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="header"></div>
+        <div id="sidebar"></div>
+        <div id="content">
+            <asp:Table ID="Table1" runat="server">
+                <asp:TableRow>
+                    <asp:TableCell>
+                        <asp:Label ID="lblID" runat="server" Text="Enter author ID: "></asp:Label>
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:TextBox ID="txtID" runat="server"></asp:TextBox>
+                    </asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell>
+                        <asp:Label ID="lblLname" runat="server" Text="Enter Last name: "></asp:Label>
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:TextBox ID="txtLname" runat="server"></asp:TextBox>
+                    </asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell>
+                        <asp:Label ID="lblFname" runat="server" Text="Enter Full name: "></asp:Label>
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:TextBox ID="txtFname" runat="server"></asp:TextBox>
+                    </asp:TableCell>
+                </asp:TableRow>
+                
+            </asp:Table>
+            <asp:Button ID="Button1" runat="server" Text="Save" OnClick="Button1_Click"  />
+            <asp:GridView ID="GridView1" runat="server" ></asp:GridView>
+         </div>
+    </form>
+</body>
+</html>
---
+++ WriteXML/default.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace WriteXML
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            ds.ReadXml(Server.MapPath("Authors.xml"));
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            writeXML();
+        }
+
+        protected void writeXML()
+        {
+            String authID = txtID.Text;
+            String authFname = txtFname.Text;
+            String authLname = txtLname.Text;
+
+            if (File.Exists(Server.MapPath("Authors.xml")) == false)
+            {
+                XmlWriterSettings xmlWriterSettings = new XmlWriterSettings();
+                xmlWriterSettings.Indent = true;
+                xmlWriterSettings.NewLineOnAttributes = true;
+                using (XmlWriter xmlWriter = XmlWriter.Create(Server.MapPath("Authors.xml"), xmlWriterSettings))
+                {
+                    xmlWriter.WriteStartDocument();
+                    xmlWriter.WriteStartElement("authorslist");
+
+                    xmlWriter.WriteStartElement("authors");
+                    xmlWriter.WriteElementString("au_id", authID);
+                    xmlWriter.WriteElementString("au_lname", authLname);
+                    xmlWriter.WriteElementString("au_fname", authFname);
+                    xmlWriter.WriteEndElement();
+
+                    xmlWriter.WriteEndElement();
+                    xmlWriter.WriteEndDocument();
+                    xmlWriter.Flush();
+                    xmlWriter.Close();
+                }
+            }
+            else
+            {
+                XDocument doc = XDocument.Load(Server.MapPath("Authors.xml"));
+                XElement root = new XElement("authors");
+                root.Add(new XElement("au_id", authID));
+                root.Add(new XElement("au_lname", authLname));
+                root.Add(new XElement("au_fname", authFname));
+                doc.Element("authorslist").Add(root);
+                doc.Save(Server.MapPath("Authors.xml"));
+            }
+        }
+    }
+}
